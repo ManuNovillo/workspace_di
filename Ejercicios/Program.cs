5c24a63a49570ecdf90ffae54e16428de07ed16c
Manuel <manuelnovilloinformatica@gmail.com>
2024-10-07 10:49:20 +0200

    oremos

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -1,6 +1,5 @@
 ﻿
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -16,9 +15,10 @@ namespace Casa
         //Ejercicio 1
         static void Ejercicio1()
         {
+            Console.WriteLine("Ejercicio 1");
             Console.WriteLine("Introduce un número para ver si es primo");
             String introducido = Console.ReadLine();
-            bool intento = int.TryParse(introducido,out int num);
+            bool intento = int.TryParse(introducido, out int num);
             if (!intento) Console.WriteLine("No es un número");
             else Console.WriteLine(EsPrimo(num) ? "Es primo" : "No es primo");
         }
@@ -27,7 +27,6 @@ namespace Casa
         /// </summary>
         /// <param name="num">El número a evaluar</param>
         /// <returns>true si es primo, false si no es primo</returns>
-        /// <exception cref="ArgumentException">Si hay lados negativos</exception>
         static bool EsPrimo(int num)
         {
             //Casos especiales
@@ -51,6 +50,7 @@ namespace Casa
 
         static void Ejercicio2()
         {
+            Console.WriteLine("Ejercicio 2");
             Console.WriteLine("Introduce las longitudes del triangulo");
             String lado1s = Console.ReadLine();
             String lado2s = Console.ReadLine();
@@ -77,9 +77,9 @@ namespace Casa
         static String TipoTriangulo(float a, float b, float c)
         {
             //Ordenar por longitudes para poder comparar el lado más largo con los otros dos más cortos
-            if (a <= 0 || b <= 0 || c <= 0) throw new ArgumentException("Los lados no pueden ser menores o iguales a 0");
             float[] longitudes = { a, b, c };
             Array.Sort(longitudes);
+            if (longitudes[2] >= longitudes[0] + longitudes[1]) return "No es un triangulo";
             if (longitudes[2] * longitudes[2] == longitudes[0] * longitudes[0] + longitudes[1] * longitudes[1]) return "Es un triángulo rectángulo";
             if (longitudes[2] * longitudes[2] > longitudes[0] * longitudes[0] + longitudes[1] * longitudes[1]) return "Es un triángulo obtusángulo";
             if (longitudes[2] * longitudes[2] < longitudes[0] * longitudes[0] + longitudes[1] * longitudes[1]) return "Es un triángulo acutángulo";
@@ -90,15 +90,20 @@ namespace Casa
         //Ejercicio 3
         static void Ejercicio3()
         {
+            Console.WriteLine("Ejercicio 3");
             Console.WriteLine("Introduce qué número de la serie de Fibonnaci quieres");
             String introducido = Console.ReadLine();
             bool intento = int.TryParse(introducido, out int num);
             if (!intento) Console.WriteLine("No es un número");
             else
             {
-                Console.WriteLine($"Recursivo:{FibonacciRecursivo(num)}");
-                Console.WriteLine("Serie hasta ese número: ");
-                Fibonacci(num);
+                if (num >= 1)
+                {
+                    Console.WriteLine($"Recursivo:{FibonacciRecursivo(num)}");
+                    Console.WriteLine("Serie hasta ese número: ");
+                    Fibonacci(num);
+                }
+                else Console.WriteLine("No puede ser menor que 1");
             }
         }
 
@@ -106,10 +111,8 @@ namespace Casa
         /// Escribe en consola todos los números de la serie de Fibonacci hasta la posición <c><paramref name="index"/></c>
         /// </summary>
         /// <param name="index"></param>
-        /// <exception cref="ArgumentException"></exception>
         static void Fibonacci(int index)
         {
-            if (index < 1) throw new ArgumentException("index no puede ser menor que 1");
             int a = 1;
             int b = 1;
             int c = 1;
@@ -126,15 +129,15 @@ namespace Casa
             if (index != 1) Console.WriteLine(c);
         }
 
+
+
         /// <summary>
         /// Devuelve el número en la posición <c><paramref name="index"/></c> de la serie de Fibonacci
         /// </summary>
         /// <param name="index">El puesto del número que se quiere calcular</param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentException">Si <c><paramref name="index"/></c> < 0</exception>
+        /// <returns>El número en la posición <c><paramref name="index"/></c> de la serie de Fibonacci</returns>
         static int FibonacciRecursivo(int index)
         {
-            if (index < 1) throw new ArgumentException("index no puede ser menor que 1");
             //Casos base
             if (index == 1 || index == 2) return 1;
             //Caso recursivo
@@ -145,6 +148,28 @@ namespace Casa
 
         static void Ejercicio4()
         {
+            Console.WriteLine("Ejercicio 4");
+            int[][] matriz = {
+                new int[] {1, 2, 3, 4},
+                new int[] {5, 6},
+                new int[] {7, 8, 9, 10, 11},
+                new int[] {11, 12, 13}
+            };
+            Console.WriteLine("Introduce la fila de la esquina superior izquierda");
+            String filaString = Console.ReadLine();
+            Console.WriteLine("Introduce la columna de la esquina superior izquierda");
+            String columnaString = Console.ReadLine();
+            int fila, columna;
+            //Intentar convertir a número, si se puede ejecutar método, si no, imprimir que hay un error 
+            if (int.TryParse(filaString, out fila) && int.TryParse(columnaString, out columna))
+            {
+                int[] submatriz = GetSubMatriz(matriz, fila, columna);
+                foreach (int num in submatriz)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            else Console.WriteLine("Revisa los datos introducidos");
 
         }
         /// <summary>
@@ -155,18 +180,17 @@ namespace Casa
         /// <param name="matriz">La matriz de la que se quiere sacar una submatriz</param>
         /// <param name="a">Primera coordenada de la submatriz, es decir, primera posición de <c><paramref name="matriz"/></c></param>
         /// <param name="b">Segunda coordenada de la submatriz, es decir, Segunda posición de <c><paramref name="matriz"/></c></param>
-        /// <returns></returns>
-        static int[] GetSubMatriz(int[,] matriz, int a, int b)
+        /// <returns>Un array unidimensional con los contenidos de la submatriz</returns>
+        static int[] GetSubMatriz(int[][] matriz, int a, int b)
         {
-            if (matriz is null) throw new ArgumentNullException("matriz no puede ser nulo");
             //Crear una lista para guardar los números en ella, pues no se sabe cuántos habrá que guardar
             List<int> list = new List<int>();
             //Recorrer la matriz a partir de las coordenadas aportadas
             for (int i = a; i < matriz.Length; i++)
             {
-                for (int j = b; j < matriz.GetLength(1); j++)
+                for (int j = b; j < matriz[i].Length; j++)
                 {
-                    list.Add(matriz[i, j]);
+                    list.Add(matriz[i][j]);
                 }
             }
             return list.ToArray();
@@ -175,17 +199,18 @@ namespace Casa
         //Ejercicio 5
         static void Ejercicio5()
         {
-
+            Console.WriteLine("Ejercicio 5");
+            Console.WriteLine("Introduce el texto al que le quieres quitar los espacios");
+            String texto = Console.ReadLine();
+            Console.WriteLine(EliminarEspacios(texto));
         }
         /// <summary>
         /// Devuelve <c><paramref name="s"/></c> pero sin cualquier caracter de espacio (" ")
         /// </summary>
         /// <param name="s">El String al que se le quieren quitar los espacios</param>
         /// <returns>Un String idéntico a <c>s</c> pero sin espacios</returns>
-        /// <exception cref="ArgumentNullException">Si <c>s</c> es nulo</exception>
         static String EliminarEspacios(String s)
         {
-            if (s is null) throw new ArgumentNullException("s no puede ser nulo");
             //Crear un StringBuilder para ir metiendo los caracteres para luego construir un String
             StringBuilder stringBuilder = new StringBuilder();
             foreach (char c in s)
@@ -200,7 +225,16 @@ namespace Casa
         //Ejercicio 6
         static void Ejercicio6()
         {
-
+            Console.WriteLine("Ejercicio 6");
+            Console.WriteLine("Introduce el número en binario");
+            String num = Console.ReadLine();
+            int[] numArray = new int[num.Length];
+            for (int i = 0; i < num.Length; i++)
+            {
+                numArray[i] = int.Parse(num[i].ToString());
+            }
+            Console.WriteLine($"Como array: {BinToDec(numArray)}");
+            Console.WriteLine($"Como String: {BinToDec(num)}");
         }
 
         /// <summary>
@@ -213,7 +247,6 @@ namespace Casa
         /// </summary>
         /// <param name="array">El número a convertir, cada cifra en una posición del array</param>
         /// <returns>El número convertido a decimal. Si <c><paramref name="array"/></c> está vacío, devuelve 0</returns>
-        /// <exception cref="ArgumentException">Si <c>array</c> contiene números que no sean 0 o 1.</exception>
         static int BinToDec(int[] array)
         {
             //Variable donde iremos sumando para tener el resultado final
@@ -226,10 +259,7 @@ namespace Casa
                 {
                     //array.Length - 1 - i da el resultado al que queremos elevar. 0 para la primera cifra, 1 para la segunda, array.Length - 1 para la primera
                     num += (int)Math.Pow(2, array.Length - 1 - i);
-
-                    //Si no es 1 y tampoco es 0, lanzar ArgumentException
                 }
-                else if (array[i] != 0) throw new ArgumentException("array solo puede contener 0 y 1");
             }
             return num;
         }
@@ -253,7 +283,7 @@ namespace Casa
             for (int i = numBinario.Length - 1; i >= 0; i--)
             {
                 //Si es 1, hay que sumar
-                if (numBinario[i].Equals("1"))
+                if (numBinario[i] == '1')
                 {
                     //array.Length - 1 - i da el resultado al que queremos elevar. 0 para la primera cifra, 1 para la segunda, array.Length - 1 para la primera
                     num += (int)Math.Pow(2, numBinario.Length - 1 - i);
@@ -268,8 +298,19 @@ namespace Casa
 
         static void Ejercicio7()
         {
-
+            Console.WriteLine("Ejercicio 7");
+            Console.WriteLine("Introduce una oracion o palabra");
+            String s1 = Console.ReadLine();
+            Console.WriteLine("Introduce otra oración o palabra");
+            String s2 = Console.ReadLine();
+            Console.WriteLine(EsPalindromo(s1, s2) ? "Son palindromos" : "No son palindromos");
         }
+        /// <summary>
+        /// Devuelve true si <c><paramref name="s1"/></c> y <c><paramref name="s2"/></c> son palíndromos, false si no.
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns>true si <c><paramref name="s1"/></c> y <c><paramref name="s2"/></c> son palíndromos, false si no.</returns>
         static bool EsPalindromo(String s1, String s2)
         {
             return Reverse(s1).Equals(s2);
@@ -278,7 +319,7 @@ namespace Casa
         /// Da la vuelta a <c><paramref name="s"/></c> de manera recursiva
         /// </summary>
         /// <param name="s"></param>
-        /// <returns></returns>
+        /// <returns>El String dado la vuelta</returns>
         static String Reverse(String s)
         {
             if (s.Length == 0 || s.Length == 1) return s;
@@ -290,7 +331,18 @@ namespace Casa
         //Ejercicio 8
         static void Ejercicio8()
         {
-
+            Console.WriteLine("Ejercicio 8");
+            Console.WriteLine("Introduce el precio del bote");
+            String precioString = Console.ReadLine();
+            Console.WriteLine("Introduce la cantidad que vas a pagar");
+            String introducidoString = Console.ReadLine();
+            float precio, introducido;
+            //Intentar convertir a número, si se puede ejecutar método, si no, imprimir que hay un error 
+            if (float.TryParse(precioString, out precio) && float.TryParse(introducidoString, out introducido))
+            {
+                GetCambio(precio, introducido);
+            }
+            else Console.WriteLine("Revisa los datos introducidos");
         }
         /// <summary>
         /// Imprime en consola la cantidad de monedas de cada tipo a dar para el resto, o "Error" si <c><paramref name="cantidad"/></c> < <c><paramref name="precio"/></c>
@@ -341,21 +393,49 @@ namespace Casa
         //Ejercicio 9
         static void Ejercicio9()
         {
+            Console.WriteLine("Ejercicio 9");
+            int[,] matriz = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+            };
+            Console.WriteLine("Introduce cuánto te quieres mover en la primera dimensión");
+            String movPrimDimString = Console.ReadLine();
+            Console.WriteLine("Introduce cuánto te quieres mover en la segunda dimensión");
+            String movSegDimString = Console.ReadLine();
+
+            int movePrimDim, moveSegDim;
+            //Intentar convertir a número, si se puede ejecutar método, si no, imprimir que hay un error 
+            if (int.TryParse(movPrimDimString, out movePrimDim) && int.TryParse(movSegDimString, out moveSegDim))
+            {
+                int[,] toroide = Toroide(matriz, movePrimDim, moveSegDim);
+                for (int i = 0; i < toroide.GetLength(0); i++)
+                {
+                    for (int j = 0; j < toroide.GetLength(1); j++)
+                    {
+                        Console.Write($"{toroide[i, j]}, ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else Console.WriteLine("Revisa los datos introducidos");
+
 
         }
         /// <summary>
         /// Desplaza cada elemento de <c><paramref name="matriz"/></c> veces en el eje vertical y 
         /// <c><paramref name="moveSegundaDim"/></c> en el eje horizontal
         /// </summary>
-        /// <param name="matriz"></param>
-        /// <param name="movePrimeraDim"></param>
-        /// <param name="moveSegundaDim"></param>
-        /// <returns></returns>
+        /// <param name="matriz">La matriz que se quiere modificar</param>
+        /// <param name="movePrimeraDim">El movimiento vertical. Positivo es hacia abajo, negativo es hacia arriba</param>
+        /// <param name="moveSegundaDim">El movimiento horizontal. Positivo es hacia la derecha, negativo es hacia la izquierda</param>
+        /// <returns>La matriz desplazada</returns>
         static int[,] Toroide(int[,] matriz, int movePrimeraDim, int moveSegundaDim)
         {
             //Crear una nueva matriz para devolver
             int[,] toroide = new int[matriz.GetLength(0), matriz.GetLength(1)];
 
+            //Recorrer la matriz
             for (int i = 0; i < matriz.GetLength(0); i++)
             {
                 for (int j = 0; j < matriz.GetLength(1); j++)
@@ -380,7 +460,7 @@ namespace Casa
         /// Si son negativos, son hacia arriba o hacia la izquierda</param>
         /// <param name="eje">0 para vertical, 1 para horizontal</param>
         /// <param name="posOriginal">La posición donde se encuentra originalmente en ese eje</param>
-        /// <returns></returns>
+        /// <returns>La nueva coordenada en ese eje</returns>
         private static int getNuevaCoord(int[,] matriz, int movimientos, int eje, int posOriginal)
         {
             //Determinar si está yendo en el sentido positivo o el negativo
@@ -443,6 +523,9 @@ namespace Casa
                 case 9:
                     Ejercicio9();
                     break;
+                case 10:
+                    EjercicioExtra();
+                    break;
                 default:
                     Console.WriteLine("No es un número válido");
                     break;
@@ -455,19 +538,51 @@ namespace Casa
         static int Menu()
         {
             Console.WriteLine("Elige un ejercicio:\n" +
-                              "" +
-                              "" +
-                              "" +
-                              "" +
-                              "" +
-                              "" +
-                              "" +
-                              "" +
-                              "" +
-                              "");
+                              "1. Numero primo\n" +
+                              "2. Triangulo\n" +
+                              "3. Fibonacci\n" +
+                              "4. Submatriz\n" +
+                              "5. Quitar espacios\n" +
+                              "6. Binario a decimal\n" +
+                              "7. Palindromo\n" +
+                              "8. Cambio\n" +
+                              "9. Toroide\n" +
+                              "10. Numero perfecto"
+                              );
             String seleccion = Console.ReadLine();
             bool intento = int.TryParse(seleccion, out int num);
             return intento ? num : -1;
+        }
+
+        static void EjercicioExtra()
+        {
+            Console.WriteLine("Ejercicio Extra");
+            Console.WriteLine("Introduce un número");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(EsPerfecto(num) ? "Es perfecto" : "No es perfecto");
+        }
+
+        /// <summary>
+        /// Devuelve true si <c><paramref name="num"/></c> es perfecto, false si no.
+        /// </summary>
+        /// <param name="num">El numero a evauluar</param>
+        /// <returns>true si <c><paramref name="num"/></c> es perfecto, false si no.</returns>
+        static bool EsPerfecto(int num)
+        {
+            //Lista donde ir guardando los divisores
+            List<int> list = new List<int>();
+            for (int i = 1; i <= num / 2; i++)
+            {
+                //Si el resto es cero, es divisible, y por tanto lo añadimos a la lista
+                if (num % i == 0) list.Add(i);
+            }
+            //Sumar los divisores
+            int suma = 0;
+            foreach (int i in list)
+            {
+                suma += i;
+            }
+            return suma == num;
         }
     }
 }
