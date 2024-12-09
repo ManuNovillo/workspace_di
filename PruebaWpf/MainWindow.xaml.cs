61daaec758523ab8929f7fb86845e44084a20a7e
Manuel <manuelnovilloinformatica@gmail.com>
2024-12-09 12:37:09 +0100

    clase

--- PruebaWpf/MainWindow.xaml.cs
+++ PruebaWpf/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PruebaWpf
 {
@@ -24,5 +11,56 @@ namespace PruebaWpf
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("PULSADO BOTON");
+        }
+
+        private void Panel_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("PULSADO PANEL");
+        }
+
+        private void MarcarTodas_Checked(object sender, RoutedEventArgs e)
+        {
+            Op1.IsChecked = true;
+            Op2.IsChecked = true;
+            Op3.IsChecked = true;
+            Op4.IsChecked = true;
+        }
+
+        private void MarcarTodas_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Op1.IsChecked = false;
+            Op2.IsChecked = false;
+            Op3.IsChecked = false;
+            Op4.IsChecked = false;
+        }
+
+        private void Op_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (Op1.IsChecked == false && Op2.IsChecked == false && Op3.IsChecked == false && Op4.IsChecked == false)
+            {
+                MarcarTodas.IsChecked = false;
+            }
+            else
+            {
+                MarcarTodas.IsChecked = null;
+            }
+        }
+
+        private void Op_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Op1.IsChecked == true && Op2.IsChecked == true && Op3.IsChecked == true && Op4.IsChecked == true)
+            {
+                MarcarTodas.IsChecked = true;
+            }
+            else
+            {
+                MarcarTodas.IsChecked = null;
+            }
+        }
+
     }
 }
