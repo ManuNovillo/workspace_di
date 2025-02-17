f10432f64bf8436e3555ce2b70986d8b81fe902b
Manuel <manuelnovilloinformatica@gmail.com>
2025-02-17 14:15:05 +0100

    clase

---
+++ PimerMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PimerMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PimerMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PimerMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
